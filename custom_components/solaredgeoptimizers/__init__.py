2a59028b325b0d2831339351eec7d207d880b04b
AndrewTapp <83402277+AndrewTapp@users.noreply.github.com>
2026-01-10 16:07:03 +0000

    Add files via upload

---
+++ custom_components/solaredgeoptimizers/__init__.py
@@ -0,0 +1,58 @@
+"""The SolarEdge Optimizers Data integration."""
+from requests import ConnectTimeout, HTTPError
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+# AJT: 10-Jan-2025: Changed from absolute import to relative import to use local solaredgeoptimizers.py instead of site-packages version
+from .solaredgeoptimizers import solaredgeoptimizers
+from .const import (
+    DOMAIN,
+    LOGGER,
+)
+from .coordinator import MyCoordinator
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up SolarEdge Optimizers Data from a config entry."""
+
+    api = solaredgeoptimizers(
+        entry.data["siteid"], entry.data["username"], entry.data["password"]
+    )
+    try:
+        http_result_code = await hass.async_add_executor_job(api.check_login)
+    except (ConnectTimeout, HTTPError) as ex:
+        LOGGER.error("Could not retrieve details from SolarEdge API")
+        raise ConfigEntryNotReady from ex
+
+    if http_result_code != 200:
+        LOGGER.error("Missing details data in SolarEdge response")
+        raise ConfigEntryNotReady
+
+    hass.data.setdefault(DOMAIN, {})
+
+    # AJT: 10-Jan-2025: Pass config_entry to coordinator to enable async_config_entry_first_refresh()
+    coordinator = MyCoordinator(hass, api, True, entry)
+
+    # Fetch initial data so we have data when entities subscribe
+    #
+    # If the refresh fails, async_config_entry_first_refresh will
+    # raise ConfigEntryNotReady and setup will try again later
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
---
+++ custom_components/solaredgeoptimizers/coordinator.py
@@ -0,0 +1,123 @@
+"""Example integration using DataUpdateCoordinator."""
+from datetime import datetime, timezone
+
+import logging
+import async_timeout
+
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+from homeassistant.util import dt as dt_util
+
+from .const import (
+    DOMAIN,
+    UPDATE_DELAY,
+    CHECK_TIME_DELTA,
+)
+
+# AJT: 10-Jan-2025: Changed from absolute import to relative import to use local solaredgeoptimizers.py instead of site-packages version
+from .solaredgeoptimizers import (
+    solaredgeoptimizers,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+class MyCoordinator(DataUpdateCoordinator):
+    """My custom coordinator."""
+
+    def __init__(self, hass, my_api: solaredgeoptimizers, first_boot, config_entry=None):
+        """Initialize my coordinator."""
+        # AJT: 10-Jan-2025: Pass config_entry to parent class to enable async_config_entry_first_refresh()
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="SolarEdgeOptimizer",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=UPDATE_DELAY,
+            config_entry=config_entry,
+        )
+        self.my_api = my_api
+        self.first_boot = first_boot
+
+    async def _async_setup(self) -> None:
+        """Set up the coordinator.
+
+        Can be overwritten by integrations to load data or resources
+        only once during the first refresh.
+        """
+
+        site = await self.hass.async_add_executor_job(self.my_api.requestListOfAllPanels)
+
+        _LOGGER.info("Found all information for site: %s", site.siteId)
+        _LOGGER.info("Site has %s inverters", len(site.inverters))
+        _LOGGER.info(
+            "Adding all optimizers (%s) found to Home Assistant",
+            site.returnNumberOfOptimizers(),
+        )
+        
+
+        i = 1
+        for inverter in site.inverters:
+            _LOGGER.info("Adding all optimizers from inverter: %s", i)
+
+            device_registry = dr.async_get(self.hass)
+            device_registry.async_get_or_create(
+                config_entry_id=self.config_entry.entry_id,
+                identifiers={(DOMAIN, inverter.serialNumber)},
+                manufacturer="SolarEdge",
+                model=inverter.type,
+                name=inverter.displayName,
+            )
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(300):
+                _LOGGER.debug("Update from the coordinator")
+                data = await self.hass.async_add_executor_job(
+                    self.my_api.requestAllData
+                )
+
+                update = False
+
+                timetocheck = dt_util.utcnow() - CHECK_TIME_DELTA    # tz-aware UTC
+
+                for optimizer in data:
+                    # AJT: 10-Jan-2025: Fixed typo "measerument" to "measurement" in log message
+                    _LOGGER.debug(
+                        "Checking time: %s | Versus last measurement: %s",
+                        timetocheck,
+                        optimizer.lastmeasurement,
+                    )
+
+                    ts = optimizer.lastmeasurement
+                
+                    # SolarEdge returns a *naive* UTC timestamp – tag it as UTC
+                    if ts.tzinfo is None:
+                        ts = ts.replace(tzinfo=timezone.utc)
+                
+                    if ts > timetocheck:
+                        update = True
+                        break
+
+                if update or self.first_boot:
+                    _LOGGER.debug("We enter new data")
+                    self.first_boot = False
+                    return data
+                else:
+                    _LOGGER.debug("No new data to enter")
+                    return None
+
+        except Exception as err:
+            _LOGGER.error("Error in updating updater")
+            _LOGGER.error(err)
+            raise UpdateFailed(err)
